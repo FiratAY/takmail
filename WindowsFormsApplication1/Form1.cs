de581f688eacafcc0f084cef8b48f0936eeb533d
Fırat AY <firat.ay@ogr.sakarya.edu.tr>
2016-03-30 19:34:15 +0300

    takmail
    takmail C#  form ortamında yazılmış bir mail programıdır.

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SmtpClient client = new SmtpClient("smtp.gmail.com,587");
+            client.Credentials = new System.Net.NetworkCredential("karay0825@gmail.com", "frty-*120");
+            client.EnableSsl = true;
+            MailMessage mesaj = new MailMessage("karay0825@gmail.com", textBox1.Text);
+            mesaj.Body = textBox3.Text;
+            mesaj.Subject = textBox2.Text;
+            client.Send(mesaj);
+        }
+    }
+}
